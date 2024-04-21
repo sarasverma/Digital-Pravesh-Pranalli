8dc4f9bd9ffd744b28a855c7a9b541abe1406bfd
Saras <78587510+sarasverma@users.noreply.github.com>
2024-04-21 20:38:28 +0530

    web complete

--- pppweb/src/pages/PageNotFound.jsx
+++ pppweb/src/pages/PageNotFound.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="text-center">
-        <h1>404</h1>
-        <div>Page Not Found</div>
+    <div className="w-full flex justify-center mt-[150px]">
+      <div>
+        <img
+          src="https://c.tenor.com/A_-hcMZvYE4AAAAd/tenor.gif"
+          alt="cat_meme_tenor"
+          className="w-[300px] aspect-square "
+        />
+        <h1 className="text-3xl text-orange-500 text-center">
+          404 Page not Found
+        </h1>
+        <p className="text-center">
+          <Link
+            to="/"
+            className="text-3xl text-orange-500 underline animate-pulse"
+          >
+            Main Page 🐘
+          </Link>
+        </p>
       </div>
     </div>
   );
